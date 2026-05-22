655241b644faaef0bbb030763e5a1e8ef31e41a9
wouairness78 <84250360+wouairness78@users.noreply.github.com>
2026-05-22 20:20:42 +0200

    Add files via upload

---
+++ Fichiers/netlify/functions/vigilance.js
@@ -0,0 +1,54 @@
+exports.handler = async function () {
+
+  try {
+
+    const token =
+      process.env.METEOCONCEPT_TOKEN;
+
+    const response = await fetch(
+      `https://api.meteo-concept.com/api/forecast/daily?token=${token}&insee=78537`
+    );
+
+    const data = await response.json();
+
+    const today = data.forecast?.[0];
+
+    if (!today) {
+      throw new Error("Aucune donnée météo");
+    }
+
+    let vigilance = "Vert";
+
+    if (today.probarain > 70) {
+      vigilance = "Orange";
+    }
+
+    if (today.probarain > 90) {
+      vigilance = "Rouge";
+    }
+
+    return {
+      statusCode: 200,
+
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({
+        vigilance,
+        temperature: today.tmax,
+        wind: today.wind10m,
+        weather: today.weather
+      })
+    };
+
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
